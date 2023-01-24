0c416875342814ca8cbeb45ad5e314c76ea2a147
Manoel Tesar <manoel.tesar@gmail.com>
2023-01-24 14:17:41 +0100

    Initial commit

---
+++ spotifypy.py
@@ -0,0 +1,25 @@
+import spotipy
+import var
+from spotipy.oauth2 import SpotifyClientCredentials
+
+playlist_uri = 'spotify:playlist:3Gg7sPifer5z1h1NaBWU6f'
+testplaylist = 'spotify:playlist:5Vxi45lmduL9boFwQq0fOP'
+
+auth_manager = SpotifyClientCredentials(
+        client_id=var.client_id,
+        client_secret=var.client_secret
+    )
+
+spotify = spotipy.Spotify(auth_manager=auth_manager)
+results = spotify.playlist(testplaylist)
+
+
+counter = 1
+for track in spotify.playlist_tracks(testplaylist)["items"]:
+    track_artist = track["track"]["artists"][0]["name"]
+    album_name = track["track"]["album"]["name"]
+    track_name = track["track"]["name"]
+    track_uri = track["track"]["uri"]
+
+    print("{}: {} - {} - {}\n\t{}\n\tAudio features: {}\n".format(counter, track_artist, album_name, track_name, track_uri, spotify.audio_features(track_uri)[0]))
+    counter += 1
